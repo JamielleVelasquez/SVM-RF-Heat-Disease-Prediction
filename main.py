a94a040ac881f85ed49570e17eba769915c60b11
JamielleVelasquez <65398417+JamielleVelasquez@users.noreply.github.com>
2022-09-05 11:21:48 +0800

    SMOTE Implementation
    Imported Imbalanced learn
    Commented links to library documentation and code to create a csv of different data

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 # Before running this file, activate the venv using "sklearn-venv\Scripts\activate"
 
-from concurrent.futures import process
-import pandas as pd
+from imblearn.over_sampling import SMOTE
 from sklearn.feature_selection import SelectKBest, chi2
+import pandas as pd
+#import numpy as np
+#np.set_printoptions(threshold=np.inf, suppress=True)
 
 # Using panda.io to read the dataset
-unprocessed_data = pd.read_csv("heart.csv")
-# print(unprocessed_data)
+unprocessed_data_X = pd.read_csv("heart.csv")
+unprocessed_data_y = unprocessed_data_X.loc[:, "target"]
+#np.savetxt('unprocessed.csv', unprocessed_data_X, delimiter=',', fmt="%.0f")
 
 # Feature Selection
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectKBest.html
 feature_selection = SelectKBest(chi2, k=11)
-processed_data = feature_selection.fit_transform(
-    unprocessed_data, unprocessed_data.loc[:, "target"])
+processed_data_X = feature_selection.fit_transform(
+    unprocessed_data_X, unprocessed_data_y)
+#np.savetxt('no_smote.csv', processed_data_X, delimiter=',', fmt="%.0f")
+
 # These features had the 3 lowest chi2 scores and will be excluded:
 # fasting blood sugar > 120 mg/dl
 # resting electrocardiographic results (values 0,1,2)
 # number of major vessels (0-3) colored by flourosopy
 
-#TODO: SMOTE
+# SMOTE
+# https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.SMOTE.html
+smote_processed_data_X, smote_processed_data_y = SMOTE(
+).fit_resample(processed_data_X, processed_data_X[:, 10])
+#np.savetxt('smote.csv', smote_processed_data_X, delimiter=',', fmt="%.0f")
+
+# 27 new entries were created by SMOTE to oversample the minority
 
 #TODO: RF
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 
 #TODO: SVM
+# https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 
 #TODO: MV
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.VotingClassifier.html
 
 # Pipelines
 
---
+++ sklearn-venv/Lib/site-packages/imblearn/_min_dependencies.py
@@ -0,0 +1,58 @@
+"""All minimum dependencies for imbalanced-learn."""
+import argparse
+
+NUMPY_MIN_VERSION = "1.17.3"
+SCIPY_MIN_VERSION = "1.3.2"
+PANDAS_MIN_VERSION = "1.0.5"
+SKLEARN_MIN_VERSION = "1.1.0"
+TENSORFLOW_MIN_VERSION = "2.4.3"
+KERAS_MIN_VERSION = "2.4.3"
+JOBLIB_MIN_VERSION = "1.0.0"
+THREADPOOLCTL_MIN_VERSION = "2.0.0"
+PYTEST_MIN_VERSION = "5.0.1"
+
+# 'build' and 'install' is included to have structured metadata for CI.
+# It will NOT be included in setup's extras_require
+# The values are (version_spec, comma separated tags)
+dependent_packages = {
+    "numpy": (NUMPY_MIN_VERSION, "install"),
+    "scipy": (SCIPY_MIN_VERSION, "install"),
+    "scikit-learn": (SKLEARN_MIN_VERSION, "install"),
+    "joblib": (JOBLIB_MIN_VERSION, "install"),
+    "threadpoolctl": (THREADPOOLCTL_MIN_VERSION, "install"),
+    "pandas": (PANDAS_MIN_VERSION, "optional, docs, examples, tests"),
+    "tensorflow": (TENSORFLOW_MIN_VERSION, "optional, docs, examples, tests"),
+    "keras": (KERAS_MIN_VERSION, "optional, docs, examples, tests"),
+    "matplotlib": ("3.1.2", "docs, examples"),
+    "seaborn": ("0.9.0", "docs, examples"),
+    "memory_profiler": ("0.57.0", "docs"),
+    "pytest": (PYTEST_MIN_VERSION, "tests"),
+    "pytest-cov": ("2.9.0", "tests"),
+    "flake8": ("3.8.2", "tests"),
+    "black": ("22.3.0", "tests"),
+    "mypy": ("0.770", "tests"),
+    "sphinx": ("4.2.0", "docs"),
+    "sphinx-gallery": ("0.7.0", "docs"),
+    "numpydoc": ("1.0.0", "docs"),
+    "sphinxcontrib-bibtex": ("2.4.1", "docs"),
+    "pydata-sphinx-theme": ("0.7.2", "docs"),
+}
+
+
+# create inverse mapping for setuptools
+tag_to_packages: dict = {
+    extra: [] for extra in ["install", "optional", "docs", "examples", "tests"]
+}
+for package, (min_version, extras) in dependent_packages.items():
+    for extra in extras.split(", "):
+        tag_to_packages[extra].append("{}>={}".format(package, min_version))
+
+
+# Used by CI to get the min dependencies
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get min dependencies for a package")
+
+    parser.add_argument("package", choices=dependent_packages)
+    args = parser.parse_args()
+    min_version = dependent_packages[args.package][0]
+    print(min_version)
